2cafbe11e35d9c216e804efe6dbafdafd8d89f56
keshavalt22 <keshavkhatri35@gmail.com>
2022-04-16 15:29:01 +0530

    assignment

---
+++ block-BNaaaz/file.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+    fs.createReadStream('./readme.txt').pipe(res);
+}
+
+server.listen(5000, () => {
+    console.log('server is listening on port 5000')
+})
